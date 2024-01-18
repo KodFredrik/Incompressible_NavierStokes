63085e108b469cb415f538cb47e114797f2cd8b5
KodFredrik <82042467+KodFredrik@users.noreply.github.com>
2024-01-18 20:35:23 +0100

    Delete Lid_Driven_Cavity directory

--- Lid_Driven_Cavity/Diffusion_term.m
+++
@@ -1,8 +0,0 @@
-function DiffXi = Diffusion_term(Xi, constant,dx,dy,dz)
-%Compute diffusion term
-Xixx= diff(Xi(:,2:end-1, 2:end-1), 2, 1);
-Xiyy = diff(Xi(2:end-1,:, 2:end-1), 2, 2);
-Xizz = diff(Xi(2:end-1, 2:end-1, :),2, 3);
-DiffXi = constant.* (Xixx./(dx^2) + Xiyy./(dy^2) + Xizz./(dz^2));
-end
-
